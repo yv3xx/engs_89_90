636bdbb63c41d28f4de5e2b1ffae8182f2ab1cae
yv3xx <yefri.s.figueroa.21@dartmouth.edu>
2021-01-06 01:26:41 -0500

    Recording with set amount of time

--- js/script.js
+++ js/script.js
@@ -1,4 +1,65 @@
 
+let downloadButton = document.getElementById("downloadButton");
+let recording = document.getElementById("download");
+let logElement = document.getElementById("log");
+let video = document.getElementById('video');
+
+let recordingTimeMS = 5000;
+
+function log(msg) {
+  logElement.innerHTML += msg + "\n";
+}
+
+function wait(delayInMS) {
+  return new Promise(resolve => setTimeout(resolve, delayInMS));
+}
+
+function record(stream, lengthInMS){
+  let recorder = new MediaRecorder(stream);
+  let data = [];
+
+  recorder.ondataavailable = event => data.push(event.data);
+  recorder.start();
+  log(recorder.state + " for " + (lengthInMS/1000) + " seconds...");
+
+  let stopped = new Promise((resolve, reject) => {
+    recorder.onstop = resolve;
+    recorder.onerror = event => reject(event.name);
+  });
+
+  let recorded = wait(lengthInMS).then(
+    () => recorder.state == "recording" && recorder.stop()
+  );
+
+  return Promise.all([
+    stopped,
+    recorded
+  ])
+  .then(() => data);
+}
+
+
+var start = function(){
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+    video.srcObject = stream;
+    downloadButton.href = stream;
+    video.captureStream = video.captureStream || video.mozCaptureStream;
+    return new Promise(resolve => video.onplaying = resolve);
+  }).then(() => record(video.captureStream(), recordingTimeMS))
+  .then (recordedChunks => {
+    let recordedBlob = new Blob(recordedChunks, { type: "video/mp4" });
+    recording.src = URL.createObjectURL(recordedBlob);
+    downloadButton.href = recording.src;
+    downloadButton.download = "RecordedVideo.mp4";
+
+    log("Successfully recorded " + recordedBlob.size + " bytes of " +
+        recordedBlob.type + " media.");
+  })
+  .catch(log);
+}
 var stop = function() {
   var stream = video.srcObject;
   var tracks = stream.getTracks();
@@ -11,12 +72,10 @@ var stop = function() {
   video.srcObject = null;
 }
 
-var start = function(){
-	var video = document.getElementById('video'),
+var preview = function(){
 	 vendorUrl = window.URL || window.webkitURL;
-
 	if (navigator.mediaDevices.getUserMedia) {
-		navigator.mediaDevices.getUserMedia({ video:true })
+		navigator.mediaDevices.getUserMedia({ audio:true, video:true })
 		.then(function (stream) {
 		  video.srcObject = stream;
 		}).catch(function (error) {
@@ -25,5 +84,5 @@ var start = function(){
 	}
 }
 $(function() {
-    start();
+    preview();
 });
